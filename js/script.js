271778d64099ca78f35690061c1ca02cb0525f6b
cyrus937 <cyrillekenfack4@gmail.com>
2022-08-04 11:46:27 +0100

    first commit

---
+++ js/script.js
@@ -0,0 +1,13 @@
+/* ======================================== typing animation ============================================ */
+var typed = new Typed(".typing", {
+    strings:["Back-end Developer", "Web Designer", "Web Developer", "Graphic Designer", "Mobile Developer"],
+    typeSpeed:100,
+    BackSpeed:60,
+    loop:true
+})
+var typed = new Typed(".typing-1", {
+    strings:["Megasoft Sarl", "CENADI", "LABO IA ENSPY"],
+    typeSpeed:100,
+    BackSpeed:60,
+    loop:true
+})
